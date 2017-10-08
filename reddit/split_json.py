5eee89e21dcd4013bf24ae3fc88ce6bcbf3b6d0a
Gabriel Fair <gabrielfair@gmail.com>
2017-10-07 20:56:43 -0400

    fixed dropped comments
    Any partical matches were lost when file.read() pulled in a new byte
    chunk

--- reddit/split_json.py
+++ reddit/split_json.py
@@ -8,81 +8,91 @@ import progressbar
 from time import sleep
 import os
 
-# parser = argparse.ArgumentParser(description='Splits giant file with many JSON objects.')
-# parser.add_argument('json file', metavar='F', type=open, help='a file containing valid json' required=True)
 
 chunksize = 45000 #reddit posts have a 15000 char limit except for self-post subreddit gets 40k, so we will just make it 45k right now to include metadata.
-comment_count = 0
+current_comment_count = 0
+global_comment_count = 0
 file_count = 0
 comments_per_file = 20000
 json_pattern = re.compile(b'\{(?:[^{}])*\}')
 file_progress = 0
-read_window = (0, chunksize)  # starting with window the size of the byte sector on the disk
+chunk_remainder = 0
 output_file_contents = ''  # The new file will build in RAM before writing to disk. Limiting the number of disk bottlenecks
-
+file_size = os.path.getsize(sys.argv[1])
+bar = progressbar.ProgressBar(maxval=file_size,widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 
 def split_json(file_argument):
 	global output_file_contents
 	global chunk
-	global comment_count
+	global current_comment_count
 	global file_progress
-	global read_window
+	global chunk_remainder
 	last_chunk = False
 	global json_pattern
+	global bar
+	global global_comment_count
 
 	print("Reading file: " + file_argument + " Splitting every: " + str(comments_per_file) + " comments")
 	with open(file_argument, "rb") as file:
-		bar = progressbar.ProgressBar(maxval=os.path.getsize(file_argument),widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 		bar.start()
 		# file_map = mmap.mmap(file.fileno(), chunksize)
 		chunk = file.read(chunksize)
 		
 		while not last_chunk:
 			for match in json_pattern.finditer(chunk):
+				#print("A match has been found: \n" + str(match.group())[2:-1])
 				if match:
-					# print("Found match!: " + str(match.group())[1:])
-					if (comment_count >= comments_per_file-1):
-						output_file_contents = output_file_contents + str(match.group())[1:]# slice is to remove a stray 'b' that appears at the start of the match
-					else:
-						output_file_contents = output_file_contents + str(match.group())[1:] + '\n'	 # slice is to remove a stray 'b' that appears at the start of the match
-					chunk = file.seek(chunksize, 1)
-					comment_count += 1
-					file_progress += match.end()
+					current_comment_count = current_comment_count + 1
+					global_comment_count = global_comment_count + 1
+					output_file_contents = output_file_contents + str(match.group())[2:-1]# 
+					if (current_comment_count < comments_per_file):
+						output_file_contents = output_file_contents + '\n'
+					chunk_remainder = match.end()
+					#print("current_comment_count: "+ str(current_comment_count) + " sys.getsizeof(str(match.group())): " + str(sys.getsizeof(str(match.group())))+ " file_progress: "+ str(file_progress)+'\n')
 				else:
 					print("ERROR: no match found in: \n" + chunk)
 					exit()
 
-				if (comment_count >= comments_per_file):
-					bar.update(file_progress)
+				if (current_comment_count >= comments_per_file):
 					write_file(file_argument, file_count, output_file_contents)
-					output_file_contents = ""
+					output_file_contents = ''
 
-			chunk = grab_new_chunk(file,chunksize)
+			chunk = chunk[chunk_remainder:] + grab_new_chunk(file,chunksize)
 			if not chunk or chunk == "":
-				print("arrived at last_chunk")
 				last_chunk = True
 				break
 		write_file(file_argument, file_count, output_file_contents)
 		bar.finish()
+		print("Bytes successfully read:  "+ str(int(file_progress)) + '/' + str(os.path.getsize(file_argument)) + ' ('+ str((file_progress//os.path.getsize(file_argument))*100) + '%)\n'+ "Total comments: "+ str(global_comment_count))
+		return
 
 
 def write_file(file_name, file_num, output):
-	global comment_count
+	global current_comment_count
 	global file_progress
-	global read_window
 	global file_count
-	f = open(file_name + '_%05d' % file_num, 'w')
+	global bar
+	global global_comment_count
+	
+	bar.update(file_progress)
+	f = open(file_name + '_%03d' % file_num, 'w')
 	f.write(output)
 	f.close()
 	file_count += 1
-	comment_count = 0
+	current_comment_count = 0
 
 
 def grab_new_chunk(file,byte_size_of_chunk):
+	global file_progress
+	#file_progress += chunksize
+	file_progress = file.tell()
+	#file.seek(byte_size_of_chunk,0)
 	return file.read(chunksize)
 
 
 def main():
+	# parser = argparse.ArgumentParser(description='Splits giant file with many JSON objects.')
+	# parser.add_argument('json file', metavar='F', type=open, help='a file containing valid json' required=True)
 	# args = parser.parse_args()
 	split_json(sys.argv[1])
 
