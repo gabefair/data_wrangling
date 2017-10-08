40804c5a8cc0c159cb96759542dfc61ac67e449c
Gabriel Fair <gabrielfair@gmail.com>
2017-10-07 22:10:32 -0400

    added stats when done
    tried to fix the progress bar N/A error that only shows up in prd

--- reddit/split_json.py
+++ reddit/split_json.py
@@ -7,7 +7,10 @@ import sys
 import progressbar
 from time import sleep
 import os
-
+from time import clock
+import atexit
+from time import time
+from datetime import timedelta
 
 chunksize = 45000 #reddit posts have a 15000 char limit except for self-post subreddit gets 40k, so we will just make it 45k right now to include metadata.
 current_comment_count = 0
@@ -18,8 +21,8 @@ json_pattern = re.compile(b'\{(?:[^{}])*\}')
 file_progress = 0
 chunk_remainder = 0
 output_file_contents = ''  # The new file will build in RAM before writing to disk. Limiting the number of disk bottlenecks
-file_size = os.path.getsize(sys.argv[1])
-bar = progressbar.ProgressBar(maxval=file_size,widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+file_size = 0
+bar = object()
 
 def split_json(file_argument):
 	global output_file_contents
@@ -31,11 +34,14 @@ def split_json(file_argument):
 	global json_pattern
 	global bar
 	global global_comment_count
+	global file_size
+	file_size = os.path.getsize(sys.argv[1])
+	bar = progressbar.ProgressBar(maxval=100,widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 
 	print("Reading file: " + file_argument + " Splitting every: " + str(comments_per_file) + " comments")
 	with open(file_argument, "rb") as file:
 		bar.start()
-		# file_map = mmap.mmap(file.fileno(), chunksize)
+		bar.update(1)
 		chunk = file.read(chunksize)
 		
 		while not last_chunk:
@@ -63,7 +69,9 @@ def split_json(file_argument):
 				break
 		write_file(file_argument, file_count, output_file_contents)
 		bar.finish()
-		print("Bytes successfully read:  "+ str(int(file_progress)) + '/' + str(os.path.getsize(file_argument)) + ' ('+ str((file_progress//os.path.getsize(file_argument))*100) + '%)\n'+ "Total comments: "+ str(global_comment_count))
+		print("Bytes successfully read:  "+ str(int(file_progress)) + '/' + str(os.path.getsize(file_argument)) + ' ('+ str((file_progress//os.path.getsize(file_argument))*100) + '%)')
+		print("Total files: ", file_count)
+		print("Total comments: ", global_comment_count)
 		return
 
 
@@ -71,11 +79,9 @@ def write_file(file_name, file_num, output):
 	global current_comment_count
 	global file_progress
 	global file_count
-	global bar
 	global global_comment_count
 	
-	bar.update(file_progress)
-	f = open(file_name + '_%03d' % file_num, 'w')
+	f = open(file_name + '_%04d' % file_num, 'w')
 	f.write(output)
 	f.close()
 	file_count += 1
@@ -84,17 +90,45 @@ def write_file(file_name, file_num, output):
 
 def grab_new_chunk(file,byte_size_of_chunk):
 	global file_progress
-	#file_progress += chunksize
-	file_progress = file.tell()
+	global bar
+	global file_size
+	
+	file_progress = int((file.tell()/file_size)*100)
+	bar.update(file_progress)
 	#file.seek(byte_size_of_chunk,0)
 	return file.read(chunksize)
+	
+	
+def secondsToStr(t):
+    return str(timedelta(seconds=t))
+
+line = "="*40
+def log(s, elapsed=None):
+    print(line)
+    print(secondsToStr(time()), '-', s)
+    if elapsed:
+        print("Elapsed time:", elapsed)
+    print(line)
+    print()
+
+def endlog(start):
+    end = time()
+    elapsed = end-start
+    log("End Program", secondsToStr(elapsed))
+
+def now():
+    return secondsToStr(time())
 
 
 def main():
-	# parser = argparse.ArgumentParser(description='Splits giant file with many JSON objects.')
+    # parser = argparse.ArgumentParser(description='Splits giant file with many JSON objects.')
 	# parser.add_argument('json file', metavar='F', type=open, help='a file containing valid json' required=True)
 	# args = parser.parse_args()
+	start_time = time()
+	atexit.register(endlog)
+	log("Start Program")
 	split_json(sys.argv[1])
+	endlog(start_time)
 
 
 if __name__ == '__main__':
